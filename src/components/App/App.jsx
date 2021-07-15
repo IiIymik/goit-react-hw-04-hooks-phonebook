54d9231bc091c294d13463a13073460537c026f9
IiIymik <sambotemp@gmail.com>
2021-07-15 21:34:46 +0300

    done hw

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,17 +6,15 @@ import Filter from '../Filter/Filter';
 
 
 
-export default function App() {
+export default function App () {
   const [contacts, setContacts] = useState(()=>{
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? '';
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
   });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
- 
 
    const deleteContact = contactId => {
     setContacts((state) => state.filter(contact=> contact.id !== contactId)
@@ -27,12 +25,11 @@ export default function App() {
     setContacts([...contacts, data]);
   }
 
-  
-  //  const getVisibleContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter))
-  // }
 
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter))
+  }
 
   return (
     <Container>
@@ -40,14 +37,10 @@ export default function App() {
       <Form onSubmit={formSubmit} data={contacts} />
       <TitleBook>Contacts</TitleBook>
       <Filter onChange={(e) => setFilter(e.currentTarget.value)} value={filter}/>
-      <ContactsList contacts={contacts} onDeleteContact={deleteContact} />
+      <ContactsList contacts={getVisibleContacts()} onDeleteContact={deleteContact} />
     </Container>
   )
 }
 
 
 
-// {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
